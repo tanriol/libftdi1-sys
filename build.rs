c820c2cf543b98fdfe7b4392bff2d4c2803e5f5b
William D. Jones <thor0505@comcast.net>
2020-03-25 02:11:12 -0400

    Convert bindings to be generated automatically in build.rs.

--- build.rs
+++ build.rs
@@ -1,9 +1,33 @@
+use bindgen;
+use std::env;
+use std::path::PathBuf;
+
 #[cfg(not(windows))]
 fn main() {
     pkg_config::find_library("libftdi1").unwrap();
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
 
 #[cfg(windows)]
 fn main() {
     vcpkg::find_package("libftdi1").unwrap();
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
---
+++ wrapper.h
@@ -0,0 +1,1 @@
+#include <libftdi1/ftdi.h>

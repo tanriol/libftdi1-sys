b19ef77600369d695ee6d8931ec98bce0881af00
William D. Jones <thor0505@comcast.net>
2020-03-25 04:21:51 -0400

    Make sure generated bindings match previous versions in enum format/blacklists.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! libftdi1 is a library for working with FTDI chips like FT232BM, FT245BM,
+//! FT2232C, FT2232D, FT245R, FT232H, FT230X The documentation for it is
+//! available at http://www.intra2net.com/en/developer/libftdi/documentation/
+//!
+//! This wrapper is based on the rust-bindgen output for libftdi 1.4
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]

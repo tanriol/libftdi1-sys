0e937fa83fa3b8dacd26793b86f51a235a3f1827
William D. Jones <thor0505@comcast.net>
2020-03-26 02:02:14 -0400

    Gate vcpkg to Windows and MSVC Rust only.

--- build.rs
+++ build.rs
@@ -3,18 +3,18 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    #[cfg(not(windows))]
+    #[cfg(not(all(windows, target_env="msvc")))]
     pkg_config::Config::new().atleast_version("1.4").probe("libftdi1").unwrap();
-    #[cfg(windows)]
+    #[cfg(all(windows, target_env="msvc"))]
     vcpkg::find_package("libftdi1").unwrap();
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .default_enum_style(bindgen::EnumVariation::Rust{ non_exhaustive : true })
         .rustfmt_bindings(true)
-        .whitelist_function("ftdi_.*") 
-        .whitelist_type("ftdi_.*") 
-        .whitelist_type("libusb_.*") 
+        .whitelist_function("ftdi_.*")
+        .whitelist_type("ftdi_.*")
+        .whitelist_type("libusb_.*")
         .blacklist_type("timeval")
         .blacklist_type("__.*")
         .raw_line("pub type timeval = libc::timeval;")

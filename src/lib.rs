84e4e70e6b686fd8e3cb8d9e89a57ec76f606f26
William D. Jones <thor0505@comcast.net>
2020-03-31 03:12:08 -0400

    Add feature for using pre-generated bindings.

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,16 @@
 //! FT2232C, FT2232D, FT245R, FT232H, FT230X The documentation for it is
 //! available at http://www.intra2net.com/en/developer/libftdi/documentation/
 //!
-//! This wrapper is based on the rust-bindgen output for libftdi 1.4
-
+//! This wrapper was generated using rust-bindgen for libftdi 1.4.
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+cfg_if::cfg_if! {
+    if #[cfg(feature = "bindgen")] {
+        include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+    } else {
+        mod pregenerated;
+        pub use pregenerated::*;
+    }
+}

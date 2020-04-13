7a9d95cfd1991a74835d24aa1bb2145f8ccfa9e6
Denis Lisov <dennis.lissov@gmail.com>
2020-04-13 18:56:16 +0300

    sys-vendored-lgpl: add windows support
    Not fully tested, but at least the build script seems to compile.

--- sys-vendored-lgpl/build.rs
+++ sys-vendored-lgpl/build.rs
@@ -1,17 +1,33 @@
 // SPDX-License-Identifier: MIT
 
+use std::path::PathBuf;
+
 fn main() {
     // Find include locations
-    let libusb = pkg_config::Config::new()
-        .probe("libusb-1.0")
-        .expect("libusb is required for libftdi");
+    let include_paths = link_and_get_include_paths();
 
     let mut libftdi = cc::Build::new();
-    for path in libusb.include_paths {
+    for path in include_paths {
         libftdi.include(path);
     }
     libftdi
         .files(&["libftdi/src/ftdi.c", "libftdi/src/ftdi_stream.c"])
         .include(".")
         .compile("ftdi1-vendored");
+}
+
+cfg_if::cfg_if! {
+    if #[cfg(all(windows, target_env="msvc"))] {
+        fn link_and_get_include_paths() -> Vec<PathBuf> {
+            let libusb = vcpkg::probe_library("libusb-1.0")
+                .expect("libusb is required for libftdi");
+            libusb.include_paths
+        }
+    } else {
+        fn link_and_get_include_paths() -> Vec<PathBuf> {
+            let libusb = pkg_config::probe_library("libusb-1.0")
+                .expect("libusb is required for libftdi");
+            libusb.include_paths
+        }
+    }
 }
